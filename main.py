cf2159db01abe79fa449ac77c113598b2521055f
Arttu Kuikka <75498768+ArttuKuikka@users.noreply.github.com>
2023-11-18 17:00:49 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+import requests
+import json
+import sys
+import datetime
+from os.path import exists
+import RPi.GPIO as GPI
+
+def aanesta(url, token, taso):
+    useSsl = True
+    #debug aikana älä käytä ssl
+    if sys.gettrace() is not None:
+        useSsl = False
+
+    headers = {'Authorization': 'Bearer ' + token}
+    response = requests.post(url + "?taso=" + str(taso), headers=headers, verify=useSsl)
+
+    if(response.status_code is not 200):
+        raise Exception(f"HTTP status code exception: code {str(response.status_code)}. response: {str(response.content)}")
+    
+    if(response.status_code is 200):
+        print(f"äänestys onnistui: code:{str(response.status_code)}, taso:{taso}  ({str(datetime.datetime.now())})" )
+
+
+def main(url, token):
+   
+    GPIO.setwarnings(False) # Ignore warning for now
+    GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+    GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+
+    GPIO.add_event_detect(10,GPIO.RISING,callback=aanesta(url, token, 1)) # Setup event on pin 10 rising edge
+
+    
+   
+
+
+
+if __name__ == "__main__":
+    filename = "config.json"
+
+    if(exists("config_override.json")):
+        filename = "config_override.json"
+
+
+    with open(filename, 'r') as f:
+        config = json.load(f)
+
+    webhookurl = config["webhook_url"]
+
+    try:
+        äänestys_url = str(config["base_url"]) + str(config["aanestys_url"])
+        token = config["Token"]
+        main(äänestys_url, token)
+    
+    except Exception as e:
+        print("error: " + str(e))
+        #webhook error code
---
+++ updateToken.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import json
+import sys
+import requests
+import subprocess
+
+config = ""
+with open('config.json', 'r') as f:
+    config = json.load(f)
+    f.close()
+
+newToken = ""
+
+useSsl = True
+    #debug aikana älä käytä ssl
+if sys.gettrace() is not None:
+    useSsl = False
+
+
+content = {'id': 0, 'username': config["user"], 'password': config["password"]}
+headers = {'Content-type': 'application/json'}
+response = requests.post(config["base_url"] + "/api/Authenticate/login", headers=headers, json=content, verify=useSsl)
+
+if(response.status_code is not 200):
+    msg = f"Error while refreshing token: code:{response.status_code} content: {response.content}"
+    #webhook error
+    raise Exception(msg)
+        
+
+responseJson = json.loads(response.content)
+newToken = responseJson["token"]
+
+print('new token: ' + newToken)
+config["Token"] = newToken
+
+with open('config.json', 'w') as wf:
+    json.dump(config, wf)
+    wf.close()
+print("successfully updated token")
+
+
+#tarkista toimivuus raspilla ja päivitä tarvittaessa
+subprocess.run("sudo systemctl restart ruokalistaPaate.service")
+print("successfully reloaded ruokalistapääte")
+
