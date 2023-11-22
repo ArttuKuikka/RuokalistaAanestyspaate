910b33c8bf139646a62990691301e4e432ecf810
Arttu Kuikka <75498768+ArttuKuikka@users.noreply.github.com>
2023-11-22 20:12:04 +0200

    gpiozero

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import json
 import sys
 import datetime
 from os.path import exists
-import RPi.GPIO as GPIO
+from gpiozero import Button
+from signal import pause
 
 def aanesta(url, token, taso):
     useSsl = True
@@ -24,11 +25,9 @@ def aanesta(url, token, taso):
 
 def main(url, token):
    
-    GPIO.setwarnings(False) # Ignore warning for now
-    GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-    GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-
-    GPIO.add_event_detect(10,GPIO.RISING,callback=aanesta(url, token, 1)) # Setup event on pin 10 rising edge
+    button = Button(18)
+    button.when_pressed = lambda: aanesta(url, token, 1)
+    pause()
 
     
    
